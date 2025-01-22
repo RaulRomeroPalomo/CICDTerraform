915a911b7ef7337754a09540ef00e722a25d47ea
Raúl Romero <r.raulromero@currence.nl@Rauls-MacBook-Pro.local>
2025-01-22 10:10:27 +0100

    add first version

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename      = "build/lambda.zip" # Debes subir un archivo .zip con tu código
+  source_code_hash = filebase64sha256("build/lambda.zip")
+}
